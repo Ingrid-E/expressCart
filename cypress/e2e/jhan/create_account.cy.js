c5af4cb401385c50327cc45627629e289bcc520b
Jean Pierre Cardenas Perea <jpcardenas190@hotmail.com>
2023-06-22 14:29:06 -0500

    pruebas Jean y Jhan

---
+++ cypress/e2e/jhan/create_account.cy.js
@@ -0,0 +1,79 @@
+describe('PF-008, Create an account', () => {
+
+    beforeEach(() => {
+        cy.visit('/admin/login');
+        cy.get('#email').type('shinhyejessi@gmail.com');
+        cy.get('#password').type('1234');
+        cy.get('#loginForm').click();
+        cy.get(':nth-child(7) > .nav-link').click();
+        cy.get('.float-right > .btn').click();
+    });
+
+    //User account inserted
+    /*cy.get('#usersName')
+    cy.get('#userEmail')
+    cy.get('#userPassword')
+    cy.get('#userNewForm > :nth-child(4) > .form-control')*/
+  
+    //Case 1: Don't put username
+    it('Trying to create an account without username', () => {
+        cy.get('#userEmail').type('LittleFrog21@gmail.com');
+        cy.get('#userPassword').type('1234');
+        cy.get('#userNewForm > :nth-child(4) > .form-control').type('1234');
+        cy.get('#btnUserAdd').click();
+        cy.get('h2').should('have.text','New User');
+        cy.get('#btnUserAdd').should('have.text','Create');
+    })
+
+    //Case 2: Put email without @ character
+    it('Trying to create an account with a bad email, email without @ character', () => {
+        cy.get('#usersName').type('LittleFrog21');
+        cy.get('#userEmail').type('LittleFrog21gmail.com');
+        cy.get('#userPassword').type('1234');
+        cy.get('#userNewForm > :nth-child(4) > .form-control').type('1234');
+        cy.get('#btnUserAdd').click();
+        cy.get('h2').should('have.text','New User');
+        cy.get('#btnUserAdd').should('have.text','Create');
+    })
+
+    //Case 3: Put email without a domain (.com,.es, etc)
+    it('Trying to create an account with a bad email, email that has not a domain', () => {
+        cy.get('#usersName').type('LittleFrog21');
+        cy.get('#userEmail').type('LittleFrog21@gmailcom');
+        cy.get('#userPassword').type('1234');
+        cy.get('#userNewForm > :nth-child(4) > .form-control').type('1234');
+        cy.get('#btnUserAdd').click();
+        cy.get('#notify_message').should('have.text','Failed to create user. Check inputs.');
+    })
+
+    //Case 4: Don't put password or password confirm
+    it('Trying to create an account without password', () => {
+        cy.get('#usersName').type('LittleFrog21');
+        cy.get('#userEmail').type('LittleFrog21@gmail.com');
+        cy.get('#btnUserAdd').click();
+        cy.get('h2').should('have.text','New User');
+        cy.get('#btnUserAdd').should('have.text','Create');
+    })
+
+    //Case 5: Don't put a valid password confirm
+    it('Trying to create an account without confirm password', () => {
+        cy.get('#usersName').type('LittleFrog21');
+        cy.get('#userEmail').type('LittleFrog21@gmail');
+        cy.get('#userPassword').type('1234');
+        cy.get('#userNewForm > :nth-child(4) > .form-control').type('1243');
+        cy.get('#btnUserAdd').click();
+        cy.get('h2').should('have.text','New User');
+        cy.get('#btnUserAdd').should('have.text','Create');
+    })
+
+    //Case 6: Put all valid classes
+    it('Adding all valid classes', () => {
+        cy.get('#usersName').type('LittleFrog21');
+        cy.get('#userEmail').type('LittleFrog21@gmail.com');
+        cy.get('#userPassword').type('1234');
+        cy.get('#userNewForm > :nth-child(4) > .form-control').type('1234');
+        cy.get('#btnUserAdd').click();
+        cy.get('#notify_message').should('have.text','User account inserted');
+    })
+
+  })
---
+++ cypress/e2e/jhan/free_shipping.cy.js
@@ -0,0 +1,43 @@
+describe('PF-006, Free Shipping', () => {
+    beforeEach(() => {
+      cy.visit('/');
+    });
+
+    //Note: Add products into the db and add them with their id
+
+
+    //Case 1: Normal Product and purchase lower than 100 euro
+    it('Make a purcharse with normal products lower than 100 euro', () => {
+      
+      cy.get('a[data-id="6461339d7fb7f132f09068b5"]').click();
+      
+      //cy.get(':nth-child(4) > .thumbnail > p.text-center > .btn').click();
+      cy.get('a[data-id="6460555a6bf3c230d8ffe243"]').click();
+      
+      //cy.get(':nth-child(5) > .thumbnail > p.text-center > .btn').click();
+      cy.get('a[data-id="646054d86bf3c230d8ffe242"]').click();
+      
+
+      cy.get('.navbar-nav > :nth-child(3) > .btn').click();
+      cy.get('#shipping-amount').should('have.text','£10.00');
+    })
+                                
+  
+    //Case 2: Normal Product and purchase equal or higher than 100 euro
+    it('Make a purcharse with normal products higher than 100 euro', () => {
+      cy.get('a[data-id="6461339d7fb7f132f09068b5"]').click();
+      cy.get('a[data-id="6461339d7fb7f132f09068b5"]').click();
+      cy.get('a[data-id="6460555a6bf3c230d8ffe243"]').click();
+      cy.get('a[data-id="646054d86bf3c230d8ffe242"]').click();
+      cy.get('.navbar-nav > :nth-child(3) > .btn').click();
+      cy.get('#shipping-amount').should('have.text','FREE shipping');
+    })
+  
+    //Case 3: Suscription Product
+    it('Make a purcharse with suscription products', () => {
+      cy.get('a[data-id="646135947fb7f132f09068b6"]').click();
+      cy.get('.navbar-nav > :nth-child(3) > .btn').click();
+      cy.get('#shipping-amount').should('have.text','FREE shipping');
+    })
+  
+  })
